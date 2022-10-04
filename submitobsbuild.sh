06b9edfbd13fd279ef734bb58b4f879ff97d6ba8
Udo Seidel <udoseidel@gmx.de>
2022-10-04 22:49:21 +0200

    obs test build using github actions #7

--- submitobsbuild.sh
+++ submitobsbuild.sh
@@ -7,13 +7,24 @@ export PATH
 osc co home:useidel unifictrl
 
 cd home*
+
+# clean up
+cd unifictrl 
+osc remove UniFi*.unix.zip
+rm UniFi*.unix.zip
+cd ..
+
 # copy (updated) files to working directory
 cp ../fedora-rpm/*spec unifictrl/
-rm unifictrl/UniFi*.unix.zip
 cp ../fedora-rpm/UniFi*.unix.zip unifictrl/
 
+# mark files for update (so far only needed for UniFi*.unix.zip)
+cd unifictrl
+osc add UniFi*.unix.zip
+cd ..
+
+
 # upload changed content
-osc addremove
 osc ci -m "Version update" unifictrl
 
 
