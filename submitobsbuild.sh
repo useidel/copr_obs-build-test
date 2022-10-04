4b8cf6ca2e7c498900d99d2766ce7f31a929e4da
Udo Seidel <udoseidel@gmx.de>
2022-10-04 22:38:28 +0200

    obs test build using github actions #6

--- submitobsbuild.sh
+++ submitobsbuild.sh
@@ -9,10 +9,12 @@ osc co home:useidel unifictrl
 cd home*
 # copy (updated) files to working directory
 cp ../fedora-rpm/*spec unifictrl/
+rm unifictrl/UniFi*.unix.zip
 cp ../fedora-rpm/UniFi*.unix.zip unifictrl/
 
 # upload changed content
-osc ci unifictrl
+osc addremove
+osc ci -m "Version update" unifictrl
 
 
 
