f257cef4bd5d59dc619bde3bdc6450a7e2c9eb7e
Udo Seidel <udoseidel@gmx.de>
2022-10-10 21:23:27 +0200

    switch to dummy package on OBS to keep test for other tests

--- submitobsbuild.sh
+++ submitobsbuild.sh
@@ -6,22 +6,22 @@ export PATH
 MYDATE=`date`
 
 # checkout the OSB package
-osc co home:useidel test
+osc co home:useidel dummy
 
 cd home*
 
 # copy (updated) files to working directory
 # note that the content from github is now in the upper directory
-cp ../*spec test/
-cp ../*.tar.gz test/
+cp ../*spec dummy/
+cp ../*.tar.gz dummy/
 
 # mark files for update if there are new ones
-#cd test
+#cd dummy
 #osc add *spec
 #cd ..
 
 # upload changed content
-osc ci -m "Update from $MYDATE" test
+osc ci -m "Update from $MYDATE" dummy
 
 
 
